1da9baef3135606101ad5a3e8444c9dfae8dfb6f
Andy George <andy@defined.net>
2022-12-10 16:43:09 -0600

    initial

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,26 @@
+set -x PATH $PATH /home/andygeorge/private/_dev/bin/
+#ssh-add -k ~/.ssh/k
+#ssh-agent
+clear
+
+#set -x PATH $HOME/.rbenv/bin $PATH
+#rbenv init - | source
+
+# golang setup
+set -x PATH $PATH /usr/local/go/bin
+set -x PATH $PATH /home/andygeorge/go/bin
+set -x PATH $PATH /home/andygeorge/bin
+
+# homebrew setup
+set -x PATH $PATH /home/linuxbrew/.linuxbrew/bin
+
+# chezmoi
+set -x EDITOR vim
+
+function fish_greeting
+    fortune
+end
+# 
+# function fish_prompt
+#   #
+# end
