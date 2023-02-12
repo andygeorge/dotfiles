e4b50396137fee314c9a44a5bd3fe0b290cc96ce
Andy George <andrew.m.george@gmail.com>
2023-02-12 10:56:44 -0600

    Update .config/fish/functions/ll.fish

--- dot_config/private_fish/functions/ll.fish
+++ dot_config/private_fish/functions/ll.fish
@@ -1,4 +1,4 @@
 function ll --wraps=ls --wraps=exa --description 'List contents of directory using long format'
     # ls -lh $argv
-		exa --tree --level=2 -a --long --header --accessed --git $argv
+		exa --tree --level=1 -a --long --header --accessed --git $argv
 end
