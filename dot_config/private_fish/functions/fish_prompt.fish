f481ce524d0c344bd29b9727a1ebdb320ed63372
Andy George <andygeorge@users.noreply.github.com>
2023-06-22 10:50:50 -0500

    Update .config/fish/functions/fish_prompt.fish

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -53,6 +53,10 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_color_cleanstate green --bold
     end
 
+		if not set -q fish_prompt_pwd_dir_length
+			set -g fish_prompt_pwd_dir_length 0
+		end
+
     set -l color_cwd
     set -l prefix
     set -l suffix
