7ffd397bb3316155338a9b5d6f16b9bbfb65cd53
Andy George <andygeorge@users.noreply.github.com>
2026-04-27 11:29:48 -0500

    Update .config/fish/functions/fnode.fish

--- dot_config/private_fish/functions/fnode.fish
+++ dot_config/private_fish/functions/fnode.fish
@@ -1,4 +1,8 @@
 function fnode
-  set -l tag (test -n "$argv[1]"; and echo $argv[1]; or echo latest)
-  podman run --rm -it -v ./:/root/nodestuff/ ghcr.io/andygeorge/fnode:$tag
+    set -l tag latest
+    if set -q argv[1]
+        set tag $argv[1]
+        set -e argv[1]
+    end
+    docker run --rm -it -v ./:/root/nodestuff/ $argv ghcr.io/andygeorge/fnode:$tag
 end
